4232dbb96070cc97d7405dc7798418668568d5f8
Arnob Dey <85866452+Arnob83@users.noreply.github.com>
2025-01-13 09:59:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def save_to_database(gender, married, dependents, self_employed, loan_amount, pr
     conn.commit()
     conn.close()
 
-@st.cache_data
+@st.cache
 def prediction(Credit_History, Education, ApplicantIncome, CoapplicantIncome, Loan_Amount_Term, Dependents, Property_Area):
     # Map inputs using the mappings
     Dependents = dependents_mapping[Dependents]
@@ -93,8 +93,11 @@ def prediction(Credit_History, Education, ApplicantIncome, CoapplicantIncome, Lo
         "Property_Area": Property_Area
     }])
 
+    # Scale the input data using the loaded scaler
+    input_data_scaled = scaler.transform(input_data)
+
     # Predict using the classifier
-    prediction = classifier.predict(input_data)
+    prediction = classifier.predict(input_data_scaled)
     pred_label = 'Approved' if prediction[0] == 1 else 'Rejected'
     return pred_label, input_data
 
@@ -143,3 +146,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
